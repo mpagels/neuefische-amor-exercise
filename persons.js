17fecb3c8f9e5970ad081cd1f1e546753bd6152b
Martin <office@martinpagels.de>
2021-03-27 15:51:41 +0100

    Change persons.js content

--- persons.js
+++ persons.js
@@ -1,246 +1,246 @@
 module.exports = {
   leftSide: [
     {
-      name: 'Edwin Quigley',
+      name: "Ervin O'Reilly",
       picture:
-        'https://content.fakeface.rest/male_27_571bfb342239460cf44d8b3b5ee24f0e9c240ce9.jpg',
-      interests: [false, false, false, false, true, true],
-    },
-    {
-      name: 'Jamie Ruecker',
-      picture:
-        'https://content.fakeface.rest/female_31_659039bf509063fa13776d38fc8f53d42b0f2ce3.jpg',
-      interests: [true, true, false, true, false, false],
-    },
-    {
-      name: 'Bernard Greenfelder',
-      picture:
-        'https://content.fakeface.rest/male_39_602db3f690377d82f44d1b2e0fc8747332278235.jpg',
-      interests: [true, true, true, false, false, false],
-    },
-    {
-      name: 'Sandra Windler',
-      picture:
-        'https://content.fakeface.rest/female_42_116c55a9e73f60f9f779de42fdefea3062dfe4b1.jpg',
-      interests: [false, false, true, false, false, true],
-    },
-    {
-      name: 'Ervin Jaskolski',
-      picture:
-        'https://content.fakeface.rest/male_28_28d851e6561936667ef636069e86cd390abb9d12.jpg',
-      interests: [false, false, true, true, true, false],
-    },
-    {
-      name: 'Pat Koelpin',
-      picture:
-        'https://content.fakeface.rest/female_20_918d7f3029c4e46ece26b188f350c8ac7f6bd445.jpg',
-      interests: [false, true, false, true, false, true],
-    },
-    {
-      name: 'Chester Block',
-      picture:
-        'https://content.fakeface.rest/male_30_724c2d1b505efd46d15b8acc9e3cdb1463019951.jpg',
-      interests: [true, false, true, true, false, false],
-    },
-    {
-      name: 'Meredith Jenkins',
-      picture:
-        'https://content.fakeface.rest/female_32_83d79b7bfa08bac3d1405bfd4a25d28520e0655c.jpg',
-      interests: [false, true, false, true, false, false],
-    },
-    {
-      name: 'William Beer',
-      picture:
-        'https://content.fakeface.rest/male_33_21c2a19cf77a540ffa0122fa22df7e016d17fbcb.jpg',
-      interests: [true, false, true, true, true, true],
-    },
-    {
-      name: 'Katie Quitzon',
-      picture:
-        'https://content.fakeface.rest/female_33_c925d79339db9542297388fdfe446d868dba52f5.jpg',
-      interests: [true, true, true, false, false, true],
-    },
-    {
-      name: 'Scott Considine',
-      picture:
-        'https://content.fakeface.rest/male_35_17a8b45bfeb661bd74d91f895f029402309b9ad6.jpg',
-      interests: [true, false, false, false, true, true],
-    },
-    {
-      name: 'Roxanne West',
-      picture:
-        'https://content.fakeface.rest/female_19_45cb895e592dd9368345cba7850274499ae41664.jpg',
+        'https://content.fakeface.rest/male_28_139ca1d204a9ff939ae24640074b743bccd8e308.jpg',
       interests: [true, true, false, true, true, true],
     },
     {
-      name: 'Eugene Satterfield',
+      name: 'Juana Cartwright',
       picture:
-        'https://content.fakeface.rest/male_36_6f24f8d9e85813cfbc98cf1f109b42daae77d27f.jpg',
-      interests: [true, true, false, false, false, true],
+        'https://content.fakeface.rest/female_21_ac87bc27ac1f539e685f893fa3be2fd607bf3dc1.jpg',
+      interests: [false, true, true, false, true, true],
     },
     {
-      name: 'Ethel Ledner',
+      name: 'Horace Bergstrom',
       picture:
-        'https://content.fakeface.rest/female_38_052c764c7032f882e4f6872ce9cba6fbd9458068.jpg',
-      interests: [false, false, true, true, true, false],
+        'https://content.fakeface.rest/male_42_a6bfb5b9514a7f5627162a2b8fe9e745ca34f5bb.jpg',
+      interests: [true, true, true, true, false, true],
     },
     {
-      name: 'Jean Feest',
+      name: 'Jamie Abbott',
       picture:
-        'https://content.fakeface.rest/male_38_207c37c68c9f138583d5ce976aee39407cd053c9.jpg',
-      interests: [true, true, false, false, false, true],
+        'https://content.fakeface.rest/female_35_787302c693252ee1ae251f27b0c184b210ac3bc3.jpg',
+      interests: [true, false, true, false, true, false],
     },
     {
-      name: 'Julia Abernathy',
+      name: 'Phillip Effertz',
       picture:
-        'https://content.fakeface.rest/female_25_3ddfbf3254ee51c05f82f4267b07ecb87d03e93a.jpg',
-      interests: [true, true, true, true, true, false],
+        'https://content.fakeface.rest/male_44_deb00f91885a4865367ccafe8a8f164abc792b7f.jpg',
+      interests: [true, false, false, true, true, true],
     },
     {
-      name: 'Neil Crona',
+      name: 'Esther Mueller',
       picture:
-        'https://content.fakeface.rest/male_33_efae4e4f92a383d1d66259900a8981024bf3a5ae.jpg',
-      interests: [false, false, true, true, true, false],
+        'https://content.fakeface.rest/female_40_40d5e7ead95da76ef7579ba4c624813c7cb05f67.jpg',
+      interests: [true, false, false, false, true, true],
     },
     {
-      name: 'Kelly Prohaska',
+      name: 'Kurt Ritchie',
       picture:
-        'https://content.fakeface.rest/female_42_4dacec5d97e840f72c0e742fcadf494a648ab0a8.jpg',
-      interests: [true, false, true, true, false, true],
+        'https://content.fakeface.rest/male_36_e1616cf6f7d3ecb10180e10b8275aa7970d47882.jpg',
+      interests: [false, false, false, true, false, false],
     },
     {
-      name: 'Ruben Beier',
+      name: 'Frances Quigley',
       picture:
-        'https://content.fakeface.rest/male_30_e63122a8c76525f85ce078439f4e3241a46ae925.jpg',
+        'https://content.fakeface.rest/female_30_fb30b4672f9a63a32a686c316f6b6f8ff4618f38.jpg',
+      interests: [false, true, false, true, true, true],
+    },
+    {
+      name: 'Jared Murazik',
+      picture:
+        'https://content.fakeface.rest/male_54_882246bb647030303db7909c747c61f17035c0d7.jpg',
+      interests: [true, true, true, false, true, true],
+    },
+    {
+      name: 'Lori Smith',
+      picture:
+        'https://content.fakeface.rest/female_44_f1bb929eeda79f8708cdb2305359e9d56c993b82.jpg',
+      interests: [true, true, true, false, true, true],
+    },
+    {
+      name: 'Forrest Bradtke',
+      picture:
+        'https://content.fakeface.rest/male_37_ca6d3ef7e1e6bfb68f714e7dd91bfa8becb0d3ee.jpg',
+      interests: [true, false, false, false, false, true],
+    },
+    {
+      name: 'Dixie Rath',
+      picture:
+        'https://content.fakeface.rest/female_48_a061aee8cf1691cad878b7379085361f58c96742.jpg',
+      interests: [false, false, true, false, true, true],
+    },
+    {
+      name: 'Rogelio Carter',
+      picture:
+        'https://content.fakeface.rest/male_45_110794d3bc232af98cfbd20c55e393a17dab70dd.jpg',
+      interests: [true, true, false, true, false, true],
+    },
+    {
+      name: 'Ginger Lowe',
+      picture:
+        'https://content.fakeface.rest/female_38_e17ebba83dbde8a553a681d11218794e86f2b3d3.jpg',
+      interests: [false, false, true, false, true, true],
+    },
+    {
+      name: 'Stephen Bogan',
+      picture:
+        'https://content.fakeface.rest/male_41_26bf4ad599cf0812355a623cad7c23fbd6c90c9f.jpg',
+      interests: [true, false, false, true, false, false],
+    },
+    {
+      name: 'Brandy Schulist',
+      picture:
+        'https://content.fakeface.rest/female_41_44b13bd213eac3b50703b8efb8a49424336f3146.jpg',
+      interests: [false, false, true, true, true, true],
+    },
+    {
+      name: 'Ken Runolfsson',
+      picture:
+        'https://content.fakeface.rest/male_37_669338a813f10d74e7c096154fbccc3d2b2b3f0e.jpg',
       interests: [true, true, true, false, false, true],
     },
     {
-      name: 'Velma Hauck',
+      name: 'Gina Mosciski',
       picture:
-        'https://content.fakeface.rest/female_34_1d6825ed99f1fe13bf4704a838c470b3455d3816.jpg',
-      interests: [true, true, true, false, false, false],
+        'https://content.fakeface.rest/female_30_d37e5620022606a68b8b3fa3c46c7d3e3eaf963b.jpg',
+      interests: [false, false, true, true, false, false],
+    },
+    {
+      name: 'Joe Schaden',
+      picture:
+        'https://content.fakeface.rest/male_57_17c0ddb85ebdbfa0abaac17a559b2524a9dd48b1.jpg',
+      interests: [false, false, false, true, true, false],
+    },
+    {
+      name: 'Jacquelyn Blick',
+      picture:
+        'https://content.fakeface.rest/female_38_453d28875b30a43a6d2629ebce7312778bb6ec18.jpg',
+      interests: [false, true, false, false, false, false],
     },
   ],
   rightSide: [
     {
-      name: 'Carroll Murray',
+      name: 'Justin Hamill',
       picture:
-        'https://content.fakeface.rest/male_49_f0cd76aacf52264c1bcc76b006a3f36ae5365de0.jpg',
-      interests: [true, true, false, true, false, true],
+        'https://content.fakeface.rest/male_38_b360ddb6a6173a8a3e4836680f3bd42778bbadda.jpg',
+      interests: [true, false, true, true, true, false],
     },
     {
-      name: 'Doris Harvey',
+      name: 'Olive Littel',
       picture:
-        'https://content.fakeface.rest/female_26_4465bd9c117fb2f73f72d60d653acc4db85271b6.jpg',
-      interests: [true, false, true, false, false, true],
-    },
-    {
-      name: 'Rickey Rodriguez',
-      picture:
-        'https://content.fakeface.rest/male_42_9bad7da1bfaeea837785f981232440345c36bff6.jpg',
-      interests: [false, true, false, false, false, true],
-    },
-    {
-      name: 'Lana Langworth',
-      picture:
-        'https://content.fakeface.rest/female_31_060eb1e8dc6cd7a5c1f7eb0838cbab726b972c6d.jpg',
-      interests: [false, true, true, false, false, false],
-    },
-    {
-      name: 'Ramiro Hickle',
-      picture:
-        'https://content.fakeface.rest/male_53_420e7aac679f75c50cbf92982aadc6a1c3d10a02.jpg',
-      interests: [true, false, false, true, true, true],
-    },
-    {
-      name: 'Johnnie Walter',
-      picture:
-        'https://content.fakeface.rest/female_24_e8af09fe44839080f1059948406bb437580d0bfe.jpg',
-      interests: [true, true, true, false, false, true],
-    },
-    {
-      name: 'Joshua Ruecker',
-      picture:
-        'https://content.fakeface.rest/male_20_e5d1d782af1229b898e9ceb7249d395ee1d40679.jpg',
-      interests: [false, true, false, true, false, true],
-    },
-    {
-      name: 'Tracey Heidenreich',
-      picture:
-        'https://content.fakeface.rest/female_29_53762ddb5c0e466c481f64dedf322944a839f787.jpg',
-      interests: [true, true, false, false, true, true],
-    },
-    {
-      name: 'Antonio Padberg',
-      picture:
-        'https://content.fakeface.rest/male_39_d222e6d7dfb03627e812c142151ef6a4e83fe46f.jpg',
-      interests: [true, true, false, false, true, false],
-    },
-    {
-      name: 'Jenny Heaney',
-      picture:
-        'https://content.fakeface.rest/female_44_5bab54a37e5b68b9936fe45020e5c9493a7119ff.jpg',
-      interests: [false, false, false, false, true, true],
-    },
-    {
-      name: 'Rufus Smith',
-      picture:
-        'https://content.fakeface.rest/male_36_326f447eb7b4795166d100200b21cdd4a1198d76.jpg',
-      interests: [true, true, false, false, false, true],
-    },
-    {
-      name: 'Angela Bosco',
-      picture:
-        'https://content.fakeface.rest/female_18_9d4ade0963949dde317a8243ba1fe0d46c26af15.jpg',
+        'https://content.fakeface.rest/female_48_00fc905c0ef393265f517a8d46b3cc7745bf422e.jpg',
       interests: [false, false, false, true, false, true],
     },
     {
-      name: 'Tony Breitenberg',
+      name: 'Bert Welch',
       picture:
-        'https://content.fakeface.rest/male_32_125fa9cf71b4c8f630d2de6c0120ed857c989168.jpg',
+        'https://content.fakeface.rest/male_45_dc47268b3e95752c0823fd934d1d991c9d22607d.jpg',
       interests: [false, true, false, false, false, true],
     },
     {
-      name: 'Dixie Dicki',
+      name: 'Ginger Stanton',
       picture:
-        'https://content.fakeface.rest/female_41_4f880ad63380789edb38e977effb56438c3c6811.jpg',
-      interests: [true, true, false, true, false, false],
+        'https://content.fakeface.rest/female_36_16ac756aba7a1d946b2b5a0779a96d907dd0e2d2.jpg',
+      interests: [false, false, true, true, false, true],
     },
     {
-      name: 'Matt Rodriguez',
+      name: 'Moses Baumbach',
       picture:
-        'https://content.fakeface.rest/male_49_f55ed83344b29b957b1c882c825b61109cc23d64.jpg',
-      interests: [true, true, true, false, false, false],
+        'https://content.fakeface.rest/male_34_3b472cc02768a14b6b4acc82d5b26e08711b9ae7.jpg',
+      interests: [true, true, false, true, true, true],
     },
     {
-      name: 'Kristen Reichert',
+      name: 'Gayle Labadie',
       picture:
-        'https://content.fakeface.rest/female_18_3b1d62e7fb237d8e87e944eac2a9f0b481378b1f.jpg',
-      interests: [true, true, true, true, false, true],
+        'https://content.fakeface.rest/female_24_dd078047631d4844fd556de3ca9c7998a247a48d.jpg',
+      interests: [true, false, false, false, true, false],
     },
     {
-      name: 'Dominic Boehm',
+      name: 'Jordan Adams',
       picture:
-        'https://content.fakeface.rest/male_35_849c82b5f51300aa719df2480f82aab505cbce61.jpg',
-      interests: [true, true, true, true, true, false],
+        'https://content.fakeface.rest/male_31_ea48401dceeed50095d75aebb1cc057ea464487b.jpg',
+      interests: [true, false, false, false, false, true],
     },
     {
-      name: 'Mamie Stamm',
+      name: 'Elizabeth Kerluke',
       picture:
-        'https://content.fakeface.rest/female_47_a323b8316346f3395fa6d2f308cfe9ac4cbbdaf1.jpg',
-      interests: [false, false, false, false, false, false],
+        'https://content.fakeface.rest/female_34_94a4e52885a0c381dbf8fb52eb94ba0548ca8f84.jpg',
+      interests: [true, false, false, true, false, false],
     },
     {
-      name: 'Mitchell Goldner',
+      name: 'Irving Ebert',
       picture:
-        'https://content.fakeface.rest/male_54_436d1a4bdc00410eaf5e7372c3148d6ef9d1b23f.jpg',
-      interests: [true, false, true, true, true, true],
+        'https://content.fakeface.rest/male_59_cd3e21f5f6ebc9010eba9fc4d8d08d8599bf1025.jpg',
+      interests: [false, true, false, false, false, true],
     },
     {
-      name: 'Darla Monahan',
+      name: 'Janet Graham',
       picture:
-        'https://content.fakeface.rest/female_25_3b850989534df186372a0120508557bce710ee46.jpg',
-      interests: [true, true, false, true, false, false],
+        'https://content.fakeface.rest/female_36_3840d7a005aeff7c3e364ac76aed6a51224932a0.jpg',
+      interests: [true, true, false, false, false, true],
+    },
+    {
+      name: 'Rudy Prohaska',
+      picture:
+        'https://content.fakeface.rest/male_23_ffd3e3acba18ef813b1ab6919c2af03c7dfe8c70.jpg',
+      interests: [false, true, true, false, false, true],
+    },
+    {
+      name: 'Margarita Botsford',
+      picture:
+        'https://content.fakeface.rest/female_22_8e91b9dfe376ed637d874801dcf19ade71cbd2e4.jpg',
+      interests: [false, true, true, true, false, true],
+    },
+    {
+      name: 'Andrew Kunde',
+      picture:
+        'https://content.fakeface.rest/male_42_70e0508119777817f1a77940343b2dbbc245d617.jpg',
+      interests: [true, true, true, false, true, false],
+    },
+    {
+      name: 'Ashley Johnston',
+      picture:
+        'https://content.fakeface.rest/female_22_b521202596c00b978c29350a362a0d2cf3e207f3.jpg',
+      interests: [false, true, true, false, false, false],
+    },
+    {
+      name: 'Ralph Braun',
+      picture:
+        'https://content.fakeface.rest/male_70_935fd6e131d49dd29ac06884413b928513ec2968.jpg',
+      interests: [true, false, true, true, false, false],
+    },
+    {
+      name: 'Karen Medhurst',
+      picture:
+        'https://content.fakeface.rest/female_26_e5d34d42da0b2ef41af150d6df2241aa9d3b09f8.jpg',
+      interests: [true, true, false, false, false, false],
+    },
+    {
+      name: 'Tyrone Nolan',
+      picture:
+        'https://content.fakeface.rest/male_29_5514c7831fa3f5db4cb2f2d931a82a43006fda4a.jpg',
+      interests: [false, true, true, true, true, true],
+    },
+    {
+      name: 'Laverne Spencer',
+      picture:
+        'https://content.fakeface.rest/female_34_70c0156f152fd636cfc456b395e8debaa772e844.jpg',
+      interests: [false, true, false, false, false, true],
+    },
+    {
+      name: 'Shaun Ward',
+      picture:
+        'https://content.fakeface.rest/male_26_cca32c8f905d0e8b5ccb027000ed0859f5446162.jpg',
+      interests: [false, false, true, false, true, false],
+    },
+    {
+      name: 'Melissa Kovacek',
+      picture:
+        'https://content.fakeface.rest/female_20_8832d388ca98228430c830a03a7440d28cd17296.jpg',
+      interests: [true, false, true, false, false, true],
     },
   ],
 }
