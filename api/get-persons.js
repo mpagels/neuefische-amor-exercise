0ee61e7058a9dacc9baeccb95bff56e38de2c6ed
Martin Pagels <office@martinpagels.de>
2021-03-27 18:57:19 +0100

    Allow cors

--- api/get-persons.js
+++ api/get-persons.js
@@ -1,5 +1,21 @@
 const persons = require('../persons')
 
-module.exports = (req, res) => {
+const allowCors = fn => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  return await fn(req, res)
+}
+
+
+const handler = (req, res) => {
   res.json(persons)
 }
+
+module.exports = allowCors(handler)
